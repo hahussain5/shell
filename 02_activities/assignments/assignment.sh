bd9c381dc0558c03d438c2741d4fc7344cc0b1c2
hahussain5 <hadia.t.hussain@gmail.com>
2024-11-29 00:10:20 -0500

    Update assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -41,9 +41,8 @@ cp ./data/raw/*event*.log ./data/processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr*
 rm ./data/processed/user_logs/*ipaddr*.log
-# rf -rf ./data from coworkers 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-touch inventory.txt
+touch data/inventory.txt
 ls data/processed/event_logs  > inventory.txt
 ls data/processed/server_logs  >> inventory.txt
 ls data/processed/user_logs  >> inventory.txt
